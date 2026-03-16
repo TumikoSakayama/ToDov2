578b8c6f6a4c6aed5e8820a8d38d384d6ddcc11f
TumikoSakayama <glee310597@gmail.com>
2026-03-15 19:17:10 -0600

    Completed initial backend code, added add note and delete note, will branch to a feature branch new code

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from models import Note, Category
 
 notes_blueprint = Blueprint('note', __name__)
@@ -22,3 +22,24 @@ def get_all_categories():
         'id': c.id,
         'name': c.name
     } for c in categories])
+
+@notes_blueprint.route('/api/notes', methods=['POST'])
+def create_note():
+    data = request.json()
+    new_note = Note(
+        title = data['title'],
+        description = data['description'],
+        priority = data['priority', 0],
+        category_id = data['category_id']
+    )
+    db.session.add(new_note)
+    db.session.commit()
+
+    return jsonify({'message': 'Note created successfully'}), 201
+
+@notes_blueprint.route('/api/notes/<int:note_id>', methods=['DELETE'])
+def delete_note(note_id):
+    note = Note.query.get_or_404(note_id)
+    db.session.delete(note)
+    db.session.commit()
+    return jsonify({'message': 'Note deleted'})
